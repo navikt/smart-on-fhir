82f367acc45eb82b0d91e1a9d9ccd53d3e279dff
Karl J. Overå <k@rl.run>
2025-04-29 10:17:41 +0200

    chore: put zod types into a lib so both @navikt/fhir-mock and @navikt/smart-on-fhir can use them

--- libs/fhir-zod/src/condition.ts
+++ libs/fhir-zod/src/condition.ts
@@ -6,7 +6,8 @@ export type FhirCondition = z.infer<typeof FhirConditionSchema>
 export const FhirConditionSchema = z.object({
     resourceType: z.literal('Condition'),
     id: z.string(),
-    subject: z.object({ type: z.literal('Patient'), reference: z.string() }),
+    subject: z.object({ type: z.literal('Patient').optional(), reference: z.string() }),
+    encounter: z.object({ type: z.literal('Encounter').optional(), reference: z.string() }),
     code: z.object({
         coding: z.array(CobeableConceptSchema),
     }),
