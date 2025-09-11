a29ccddd49829954978261466a076534ffbfac6f
Karl J. Overå <k@rl.run>
2025-09-11 16:12:41 +0200

    chore: add meta.profile to FhirPatient

--- src/zod/patient.ts
+++ src/zod/patient.ts
@@ -5,6 +5,7 @@ import { GeneralIdentifierSchema, NameSchema } from './common'
 export type FhirPatient = z.infer<typeof FhirPatientSchema>
 export const FhirPatientSchema = z.object({
     resourceType: z.literal('Patient'),
+    meta: z.object({ profile: z.array(z.string()) }).optional(),
     identifier: z.array(GeneralIdentifierSchema).optional(),
     name: NameSchema,
     generalPractitioner: z.array(z.object({ identifier: GeneralIdentifierSchema, display: z.string() })).optional(),
