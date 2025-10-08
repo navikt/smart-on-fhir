289a03aca26b15910e2aee33c15989275cd87c40
Karl J. Overå <k@rl.run>
2025-10-08 15:26:23 +0200

    chore: log actual system value when Organization.telecom.system crashes

--- src/zod/organization.ts
+++ src/zod/organization.ts
@@ -2,11 +2,19 @@ import * as z from 'zod'
 
 import { GeneralIdentifierSchema } from './common'
 
+const TelecomSystemSchema = z.enum(['phone', 'email'], {
+    error: (issue) => {
+        return issue.input == null
+            ? 'Telecom system is required'
+            : `Telecom system must be either phone or email, was ${issue.input}`
+    },
+})
+
 export type FhirOrganization = z.infer<typeof FhirOrganizationSchema>
 export const FhirOrganizationSchema = z.object({
     resourceType: z.literal('Organization'),
     id: z.string(),
     identifier: z.array(GeneralIdentifierSchema),
     name: z.string(),
-    telecom: z.array(z.object({ system: z.enum(['phone', 'email']), value: z.string() })),
+    telecom: z.array(z.object({ system: TelecomSystemSchema, value: z.string() })),
 })
