db835422ec1266ce0d0810413fe6d05a488c3a14
Karl J. Overå <k@rl.run>
2026-02-17 13:08:29 +0100

    feat: add telecom to Practitioner (for email)

--- src/zod/practitioner.ts
+++ src/zod/practitioner.ts
@@ -2,6 +2,13 @@ import * as z from 'zod'
 
 import { NameSchema } from '../zod'
 
+const TelecomSystemSchema = z.enum(['phone', 'fax', 'email', 'pager', 'url', 'sms', 'other'])
+const ContactPointSchema = z.object({
+    system: TelecomSystemSchema,
+    value: z.string(),
+    use: z.enum(['home', 'work', 'temp', 'old', 'mobile']).optional(),
+})
+
 export type FhirPractitionerQualification = z.infer<typeof FhirPractitionerQualificationSchema>
 const FhirPractitionerQualificationSchema = z.object({
     code: z.object({
@@ -23,5 +30,6 @@ export const FhirPractitionerSchema = z.object({
     meta: z.object({ profile: z.array(z.string()).optional() }).optional(),
     name: NameSchema,
     identifier: z.array(z.object({ system: z.string(), value: z.string() })),
+    telecom: z.array(ContactPointSchema).optional(),
     qualification: z.array(FhirPractitionerQualificationSchema).optional(),
 })
