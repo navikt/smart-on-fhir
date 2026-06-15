aa0bfa0751d4bca168c635483089f1e2997e5aeb
Karl J. Overå <k@rl.run>
2026-06-15 12:39:45 +0200

    chore: migrate from tsup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown'
+
+import pkgJson from './package.json' with { type: 'json' }
+
+export default defineConfig({
+  dts: true,
+  sourcemap: true,
+  entry: ['src/zod/index.ts', 'src/client/index.ts'],
+  define: { LIB_VERSION: JSON.stringify(pkgJson.version) },
+  target: false,
+})
