bdb524dbd7b6bf7924d420be08547d8e71cf40fd
Karl J. Overå <k@rl.run>
2026-06-15 15:50:36 +0200

    chore: set next version to 1.6.0

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,5 @@
 export default {
-    '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': ['yarn fmt', 'yarn lint:fix'],
+    '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': 'yarn fmt',
+    '*.{ts,tsx,js,jsx}': 'yarn lint:fix',
     '*.{ts,tsx}': () => ['yarn tsc'],
 }
