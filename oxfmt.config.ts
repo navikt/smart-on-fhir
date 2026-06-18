01cc269bdf8dbba21b97f76260887ea2096a1d42
Karl J. Overå <k@rl.run>
2026-06-18 14:05:23 +0200

    chore: bump dev deps
    skip release

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,28 +3,4 @@ import { defineConfig } from 'oxfmt'
 
 export default defineConfig({
     ...config,
-    sortImports: {
-        newlinesBetween: true,
-        groups: [
-            // [":NODE:", ":PACKAGE:"]
-            ['builtin', 'external'],
-            // :ALIAS:
-            'internal',
-            // ../**
-            'parent',
-            // ./**
-            'sibling',
-            // ./index
-            'index',
-        ],
-    },
-    overrides: [
-        {
-            files: ['*.md', '*.mdx'],
-            options: {
-                printWidth: 100,
-                tabWidth: 2,
-            },
-        },
-    ],
 })
