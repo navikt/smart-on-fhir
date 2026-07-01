20ef461a6152623cf2c91db842a3208fd0cdd03b
Karl J. Overå <k@rl.run>
2026-07-01 11:25:27 +0200

    feat: support 'strict audience validation' for private_key_jwt

--- src/client/smart/client-auth-method/token-endpoint.ts
+++ src/client/smart/client-auth-method/token-endpoint.ts
@@ -1,29 +1,45 @@
+import type { InitialSession } from '../../storage/schema'
+
 import { createClientAssertion } from './client-assertion'
 import type { FhirAuthMode } from './config'
 
+export type TokenRequestPayload =
+    | {
+          grant_type: 'authorization_code'
+          client_id: string
+          code: string
+          code_verifier: string
+          redirect_uri: string
+      }
+    | {
+          grant_type: 'refresh_token'
+          client_id: string
+          refresh_token: string
+      }
+
 export async function postFormEncodedTokenEndpoint(
-    tokenEndpoint: string,
-    body: URLSearchParams,
+    payload: TokenRequestPayload,
+    session: InitialSession,
     clientId: string,
     authMode: FhirAuthMode,
 ): Promise<Response> {
     if (authMode.type === 'public') {
-        return await fetch(tokenEndpoint, {
+        return await fetch(session.tokenEndpoint, {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: body,
+            body: new URLSearchParams(payload),
         })
     }
 
     switch (authMode.method) {
         case 'client_secret_post': {
-            const newBody = new URLSearchParams(body.entries())
+            const newBody = new URLSearchParams(payload)
             newBody.append('client_secret', authMode.clientSecret)
 
-            return await fetch(tokenEndpoint, {
+            return await fetch(session.tokenEndpoint, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
@@ -35,25 +51,25 @@ export async function postFormEncodedTokenEndpoint(
 
         case 'client_secret_basic': {
             const encodedCredentials = Buffer.from(`${clientId}:${authMode.clientSecret}`).toString('base64')
-            return await fetch(tokenEndpoint, {
+            return await fetch(session.tokenEndpoint, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/x-www-form-urlencoded',
                     Authorization: `Basic ${encodedCredentials}`,
                 },
-                body: body,
+                body: new URLSearchParams(payload),
             })
         }
 
         case 'private_key_jwt': {
-            const clientAssertion = await createClientAssertion(clientId, tokenEndpoint, authMode.privateKey)
+            const clientAssertion = await createClientAssertion(clientId, session, authMode)
 
-            const newBody = new URLSearchParams(body.entries())
+            const newBody = new URLSearchParams(payload)
             newBody.append('client_assertion_type', 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer')
             newBody.append('client_assertion', clientAssertion)
 
-            return await fetch(tokenEndpoint, {
+            return await fetch(session.tokenEndpoint, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
@@ -64,6 +80,6 @@ export async function postFormEncodedTokenEndpoint(
         }
 
         default:
-            throw new Error(`Invariant violation: Unknown FHIR auth mode`)
+            throw new Error(`Invariant violation: Unknown FHIR auth mode: ${authMode['type'] as string}`)
     }
 }
